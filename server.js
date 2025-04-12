61c05f9a5e86afdf475c0361cd339a5873de51c3
KONSTANTINOS ASTRIDIS <astrikon@KONSTANINOSsMBP.home>
2025-04-13 01:57:25 +0300

    Fix: listen on Cloud Run default port 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const serviceAccount = require('./serviceAccountKey.json');
 
